8fb38396d2628427865f446690ed0afcefd320d5
James Ives <iam@jamesiv.es>
2021-04-04 11:44:38 -0400

    Remove output

--- src/execute.ts
+++ src/execute.ts
@@ -1,7 +1,5 @@
 import {exec} from '@actions/exec'
 
-let output: string
-
 /** Wrapper around the GitHub toolkit exec command which returns the output.
  * Also allows you to easily toggle the current working directory.
  *
@@ -14,20 +12,11 @@ export async function execute(
   cwd: string,
   silent: boolean
 ): Promise<any> {
-  output = ''
-
   await exec(cmd, [], {
     // Silences the input unless the INPUT_DEBUG flag is set.
     silent,
     cwd,
-    listeners: {
-      stdout
-    }
   })
 
-  return Promise.resolve(output)
-}
-
-export function stdout(data: any): string | void {
-  output += data.toString().trim()
+  return Promise.resolve()
 }
