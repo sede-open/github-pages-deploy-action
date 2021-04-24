7c470c67d1c34a5a9e75cece020d5b1a4a482251
James Ives <iam@jamesiv.es>
2021-04-24 14:38:15 +0000

    Deploy Production Code for Commit 8d5d68affcec7e128765b15e355e8f66def53608 🚀

--- lib/git.d.ts
+++ lib/git.d.ts
@@ -1,3 +1,4 @@
 import { ActionInterface, Status } from './constants';
 export declare function init(action: ActionInterface): Promise<void | Error>;
+export declare function cleanup(action: ActionInterface): Promise<void>;
 export declare function deploy(action: ActionInterface): Promise<Status>;
