9cb269b0dc13f88511cafddefc0e1a40cf132edc
James Ives <iam@jamesiv.es>
2021-04-26 23:06:28 -0400

    Update git.ts

--- src/git.ts
+++ src/git.ts
@@ -193,7 +193,7 @@ export async function deploy(action: ActionInterface): Promise<Status> {
     info(`Tidying up`)
 
     await execute(
-      `git branch -D ${action.branch}`,
+      `git branch -D ${action.branch} ${action.singleCommit ? '--orphan' : ''}`,
       action.workspace,
       action.silent
     )
